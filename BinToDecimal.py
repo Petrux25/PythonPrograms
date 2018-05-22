c90fed30e18a6f30f5efd003cef3fdba25e058b7
Diego Velasquez <diesveca@hotmail.com>
2018-05-22 14:47:35 -0600

    Added fuction to convert binary string to decimal int

---
+++ BinToDecimal.py
@@ -0,0 +1,8 @@
+
+#Function : BinToDecimal
+#Recieves Binary number type string
+#Return decimal value of the string
+def BinToDecimal(BinNumber):
+    Decimal = int(BinNumber,2)
+    return Decimal
+#BinToDecimal
